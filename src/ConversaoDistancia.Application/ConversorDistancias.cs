f653bbd47d7b39e38358fcc0de22c0aa7a76f5f7
Luzimar Pereira de Oliveira <luzimar.oliveira@ndd.com.br>
2021-10-15 19:01:21 -0400

    update gitignore

--- src/ConversaoDistancia.Application/ConversorDistancias.cs
+++ src/ConversaoDistancia.Application/ConversorDistancias.cs
@@ -5,8 +5,19 @@ namespace ConversaoDistancia.Application
 {
     public static class ConversorDistancias
     {
-        public static double MilhasParaKm(double milhas) => Math.Round(milhas * 1.609, 3);
+        public static double MilhasParaKm(double milhas)
+        {
+            return Math.Round(milhas * 1.609, 3);
+        }
 
-        public static double KmParaMilhas(double km) => Math.Round(km / 1.609, 3);
+        public static double KmParaMilhas(double km)
+        {
+            return Math.Round(km / 1.609, 3);
+        }
+
+        public static double KmParaMetros(double km)
+        {
+            return Math.Round(km / 1000, 3);
+        }
     }
 }
