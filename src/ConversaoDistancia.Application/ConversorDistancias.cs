07ad1707ca014f850ab98dcc55b831f9541b63b6
Luzimar Pereira de Oliveira <luzimar.oliveira@ndd.com.br>
2021-10-15 18:45:41 -0400

    Initial commit

---
+++ src/ConversaoDistancia.Application/ConversorDistancias.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace ConversaoDistancia.Application
+{
+    public static class ConversorDistancias
+    {
+        public static double MilhasParaKm(double milhas) => Math.Round(milhas * 1.609, 3);
+
+        public static double KmParaMilhas(double km) => Math.Round(km / 1.609, 3);
+    }
+}
